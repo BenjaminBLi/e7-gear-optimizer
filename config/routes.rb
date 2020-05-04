f5e7b1ce2427ba65116ade6147e0c557f8b0dce0
Tom Zhu <tzzhu@uwaterloo.ca>
2020-05-04 16:45:41 -0400

    finish step 6 react-router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '*page', to: 'static#index', contraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
