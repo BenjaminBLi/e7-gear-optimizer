0fafcc27370fecde80898b70165989529e4c00a0
tomzyzhu <tzzhu@edu.uwaterloo.ca>
2020-05-09 02:15:28 -0400

    Revert "testing remove typography"
    This reverts commit 6e981a2e28c3f27eda3708c1caab4f2e8f18d83d.

--- app/javascript/components/src/modules/GearDisplay/components/Gear/GearView.tsx
+++ app/javascript/components/src/modules/GearDisplay/components/Gear/GearView.tsx
@@ -39,6 +39,17 @@ const SimpleCard = () => {
     return (
       <Card className={classes.root}>
         <CardContent>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
         </CardContent>
         <CardActions>
           <Button size="small">Learn More</Button>
