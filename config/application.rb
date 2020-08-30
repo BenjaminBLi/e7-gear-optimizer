84d1b04b5ebe200c500d665cb8e994d60ba76873
Tom Zhu <tzzhu@uwaterloo.ca>
2020-08-30 19:39:19 -0400

    Change config for mongoDB

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,26 @@
 require_relative 'boot'
 
-require 'rails/all'
+# Remove or comment out
+#require "rails/all"
+
+# Add this require instead of "rails/all":
+require "rails"
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+# Remove or comment out ActiveRecord and ActiveStorage:
+# require "active_record/railtie"
+# require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
