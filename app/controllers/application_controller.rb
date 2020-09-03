74255f4b66fcd8a015fa74447d89c32d2653383f
Tom Zhu <tzzhu@uwaterloo.ca>
2020-09-03 02:12:37 -0400

    small find change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?
 
     def current_user    
-        User.find_by(id: session[:user_id])
+        User.find(id: session[:user_id])
     end
     def logged_in?
         !current_user.nil?  
